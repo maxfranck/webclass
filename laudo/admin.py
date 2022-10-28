f8dbd741ef312bd70f8581737b8978bc050847db
Max Franck <max.franck@gmail.com>
2022-10-28 12:45:23 -0300

    Voltando ao ZERO

--- laudo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Documento
-
-class DocumentoAdmin(admin.ModelAdmin):
-    fields = ['amostra_id', 'oferta', 'distribuidor', 'cultura']
-    list_display = ('amostra_id', 'oferta', 'distribuidor', 'cultura')
-
-admin.site.register(Documento, DocumentoAdmin)
